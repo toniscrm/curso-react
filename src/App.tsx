738780e6b249cc1a4c85c4cbb390109681519e3b
Toni Ferrer <antoni.ferrer@scrm.lidl>
2023-06-15 12:48:24 +0200

    Leccion 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
 import "./App.css"
-import ComponenteClase from "./components/ComponenteClase"
-import ComponenteFuncional from "./components/ComponenteFuncional"
 import SaludoClase from "./components/SaludoClase"
-
-const saludo = <h1>Hola mundo</h1>
-
-const Boton = () => {
-  return <button>Soy un boton</button>
-}
 
 function App() {
   return (
     <div>
-      {saludo}
-      <Boton></Boton>
-      <ComponenteClase></ComponenteClase>
-      <ComponenteFuncional></ComponenteFuncional>
       <SaludoClase nombre="Natalia" edad={25}></SaludoClase>
     </div>
   )
